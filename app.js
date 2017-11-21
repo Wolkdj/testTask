5afdaee41d3e25878684aa84b6ba3ba981c87208
wolkdj <wolkdj@gmail.com>
2017-11-21 22:11:07 +0200

    init project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//require('@routes')(app)
+
+app.get('*', (req, res) => res.status(200).send({
+    message:"hi, im here, wake me up pls"
+}));
+
+module.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,11 @@
+require('module-alias/register')
+
+const http = require('http');
+const app = require('../app');
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
